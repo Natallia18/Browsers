1f7fb0998829c6b651a8cc342bc9dd2747392f25
Natallia18 <58847015+Natallia18@users.noreply.github.com>
2020-02-26 15:12:51 +0300

    Commit tests to branch 'BrowserTest'

---
+++ conf.js
@@ -0,0 +1,19 @@
+exports.config = {
+    framework: 'jasmine',
+    specs: ['spec.js'],
+    onPrepare: function() {
+        browser.waitForAngularEnabled(false)
+    },
+    multiCapabilities: [
+        {
+            'browserName': 'internet explorer',
+            'platform': 'ANY',
+            'version': '11'
+        }, {
+            'browserName': 'firefox'
+    }, {
+        'browserName': 'chrome'
+    }],
+    seleniumAddress: 'http://localhost:4444/wd/hub'
+};
+
