1443bc6fc78f3f8a3a8440a40cb7ac7fe48d74ae
Natallia18 <58847015+Natallia18@users.noreply.github.com>
2020-03-17 15:40:33 +0300

    File "ProjectYandex" will be added to "Browsers"

--- PageObject.js
+++ PageObject.js
@@ -1,9 +1,8 @@
 function yandex () {
     this.geoName = element(by.css('.geolink__reg'));
-    this.geoInput = element(by.id('city__front-input'));
+    this.geoInput = element(by.css('.input__control.input__input'));
     this.geoInputDropdown = element(by.xpath('//li[1]'));
     this.elseButton = element(by.xpath('//div[1]/a[9]'));
-    this.elseElements = element.all(by.css('.home-tabs__more-top')).all(by.css('.home-tabs__more-item'));
-    this.elseLowerElements = element.all(by.css('.home-link.home-tabs__more-link.home-link_black_yes')).getText();
+    this.elseElements = element.all(by.css('.home-link.home-tabs__more-link.home-link_black_yes')).getText();
 }
 module.exports = new yandex();
--- projectYandex.js
+++ projectYandex.js
@@ -7,39 +7,31 @@ describe('Locations', function () {
 
     it('should collect data for London', async function () {
         await browser.get('https://yandex.by/');
-        // await waiters.waitForelementIsPresent(yandex.geoName);
-        await browser.sleep(1000);
-        await element(by.css('.geolink__reg')).click();
-        //await browser.sleep(1000);
-        await browser.getAllWindowHandles().then(function (handles) {
-            expect(handles.length).toEqual(2);
-            browser.switchTo().window(handles[1])
-        });
-        await element(by.id('city__front-input')).sendKeys(protractor.Key.chord(protractor.Key.CONTROL, 'a'));
-        await element(by.id('city__front-input')).sendKeys(protractor.Key.DELETE);
-        await element(by.id('city__front-input')).sendKeys("Лондон");
+        await waiters.waitForelementIsPresent(yandex.geoName);
+        yandex.geoName.click();
+        await waiters.waitForelementIsPresent(yandex.geoInput);
+        yandex.geoInput.clear();
+        yandex.geoInput.sendKeys("Лондон");
         await waiters.waitForelementIsPresent(yandex.geoInputDropdown);
-        await element(by.xpath('//li[1]')).click();
+        yandex.geoInputDropdown.click();
         await waiters.waitForelementIsPresent(yandex.elseButton);
-        await element(by.xpath('//div[1]/a[9]')).click();
-        let london = await element.all(by.css('.home-link.home-tabs__more-link.home-link_black_yes')).getText();
+        yandex.elseButton.click();
+        let london = yandex.elseElements.getText();
     });
 
     it('should collect data for Paris', async function () {
-        await element(by.css('.geolink__reg')).click();
-        await browser.getAllWindowHandles().then(function (handles) {
-            expect(handles.length).toEqual(3);
-            browser.switchTo().window(handles[2])
-        });
-        await element(by.id('city__front-input')).sendKeys(protractor.Key.chord(protractor.Key.CONTROL, 'a'));
-        await element(by.id('city__front-input')).sendKeys(protractor.Key.DELETE);
-        await element(by.id('city__front-input')).sendKeys("Париж");
+        await waiters.waitForelementIsPresent(yandex.geoName);
+        yandex.geoName.click();
+        await waiters.waitForelementIsPresent(yandex.geoInput);
+        yandex.geoInput.clear();
+        await waiters.waitForelementIsPresent(yandex.geoInput);
+        yandex.geoInput.sendKeys("Париж");
         await waiters.waitForelementIsPresent(yandex.geoInputDropdown);
-        await element(by.xpath('//li[1]')).click();
+        yandex.geoInputDropdown.click();
         await waiters.waitForelementIsPresent(yandex.elseButton);
-        await element(by.xpath('//div[1]/a[9]')).click();
+        yandex.elseButton.click();
         await waiters.waitForelementIsPresent(yandex.elseElements);
-        let paris = await element.all(by.css('.home-link.home-tabs__more-link.home-link_black_yes')).getText();
+        let paris = yandex.elseElements.getText();
     });
 
     it('should compare results', async function () {
