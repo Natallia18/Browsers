66d80818ed82a1bd899f7016f855efc6c75625bf
Natallia18 <58847015+Natallia18@users.noreply.github.com>
2020-03-27 12:36:26 +0300

    Files "yandexPage" and 'Tests' will be added to "Browsers"

--- yandexPage.js
+++ yandexPage.js
@@ -1,14 +1,34 @@
-let yandex = require('./PageObject.js');
+let waiters = require('./expectations.js');
 
-describe('Locations', function () {
+function yandexPage() {
+    this.geoLink = element(by.css('.geolink__reg'));
+    this.elseButton = element(by.css('.home-tabs__more-switcher'));
+    this.elseButtonLinks = element.all(by.css('.home-tabs__more-link'));
+    this.cityInput = element(by.css('.input__input'));
+    this.cityDropdownItems = element.all(by.css('.b-autocomplete-item__reg'));
 
-    it("City", function () {
-        yandex.getYandexPage();
-        yandex.setLocation("Лондон");
-        const LondonLocation = yandex.getMore();
-        yandex.setLocation("Париж");
-        const ParisLocation = yandex.getMore();
-        expect(LondonLocation).toEqual(ParisLocation);
+    this.openYandexPage = function () {
+        browser.get('https://yandex.by/');
+    };
 
-    })
-});
+    this.setLocation = async function (city) {
+        await waiters.waitForelementIsPresent(this.geoLink);
+        this.geoLink.click();
+        await waiters.waitForelementIsClickable(this.cityInput);
+        this.cityInput.clear();
+        this.cityInput.sendKeys(city);
+        await waiters.waitForelementIsClickable(this.cityDropdownItems.get(4));
+        await browser.actions().sendKeys(protractor.Key.ENTER).perform();
+        await waiters.waitForelementIsClickable(this.geoLink);
+        await waiters.waitForelementIsClickable(this.elseButton);
+        this.elseButton.click();
+        await waiters.waitForelementIsPresent(this.elseButtonLinks.last());
+    };
+
+    this.getElements = async function () {
+        await waiters.waitForelementIsClickable(this.elseButtonLinks.last());
+        this.elseButtonLinks.getText();
+    };
+}
+
+module.exports = new yandexPage();
